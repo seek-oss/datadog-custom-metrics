08a50084ba0155f2038236ef1e0864722432fc9b
Mattias Williams <mwilliams@seek.com.au>
2023-03-28 13:22:36 +1100

    fix(LambdaExtensionClient): Export interface

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export { createTimedSpan } from './createTimedSpan';
 export { httpTracingConfig } from './httpTracing';
 
 export type { AppConfig } from './AppConfig';
+export type { LambdaExtensionMetricsClient } from './LambdaExtensionMetricsClient';
 export type { MetricsClient } from './MetricsClient';
 export type { StatsDConfig } from './createStatsDClient';
