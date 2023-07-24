6ec670f7fd9a1fa9c419cfff7384096f27a9c227
Mattias Williams <mwilliams@seek.com.au>
2023-07-24 10:59:49 +1000

    feat(createTimedSpan): Pass along `tags` from `afterCompletion`

--- src/createTimedSpan.ts
+++ src/createTimedSpan.ts
@@ -2,6 +2,10 @@ import type { MetricsClient } from './MetricsClient';
 
 type TimingMetricsClient = Pick<MetricsClient, 'increment' | 'timing'>;
 
+interface AfterCompletion {
+  tags: string[];
+}
+
 /**
  * Sends timing related metrics for an asynchronous operation
  *
@@ -18,28 +22,40 @@ export const createTimedSpan =
   async <T>(
     name: string,
     block: () => PromiseLike<T>,
-    afterCompletion?: (duration: number, success: boolean) => void,
+    afterCompletion?: (
+      duration: number,
+      success: boolean,
+      result: T | undefined,
+    ) => AfterCompletion | void,
     tags?: string[],
   ): Promise<T> => {
     const startTime = process.hrtime.bigint();
 
-    const handleCompletion = (success: boolean) => {
+    const handleCompletion = (success: boolean, result: T | undefined) => {
       const durationNanos = process.hrtime.bigint() - startTime;
       const successTag = success ? 'success' : 'failure';
       const durationMilliseconds = Number(durationNanos) / 1e6;
 
-      metricsClient.timing(`${name}.latency`, durationMilliseconds, tags);
-      metricsClient.increment(`${name}.count`, [successTag, ...(tags ?? [])]);
+      const complete = afterCompletion?.(durationMilliseconds, success, result);
 
-      afterCompletion?.(durationMilliseconds, success);
+      const tagsToAdd =
+        tags || complete?.tags
+          ? [...(tags ?? []), ...(complete?.tags ?? [])]
+          : undefined;
+
+      metricsClient.timing(`${name}.latency`, durationMilliseconds, tagsToAdd);
+      metricsClient.increment(`${name}.count`, [
+        successTag,
+        ...(tagsToAdd ?? []),
+      ]);
     };
 
     try {
       const result = await block();
-      handleCompletion(true);
+      handleCompletion(true, result);
       return result;
     } catch (e) {
-      handleCompletion(false);
+      handleCompletion(false, undefined);
       throw e;
     }
   };
