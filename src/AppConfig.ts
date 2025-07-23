7dd0582ea717c2ed55b41992fb8dc17bf16af475
Ryan Ling <ryan@outlook.com.au>
2025-07-23 11:46:09 +1000

    Soften deprecation

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -5,8 +5,15 @@ export interface AppConfig {
   /**
    * Environment of the application
    *
-   * @deprecated Rely on the `env` set by your Datadog agent; this will be the
-   * Automat or Gantry environment name at SEEK.
+   * @deprecated Review whether you can rely on the `env` set by your Datadog
+   * agent; this will be the Automat or Gantry environment name at SEEK.
+   *
+   * In some scenarios, you may still want to manually set a different
+   * environment here. Some Gantry services may have a Gantry environment name
+   * like `prod-1` and then supply a different value like `production` here.
+   * This behaviour has been retained. It results in metrics that are tagged
+   * with both `env:prod-1` and `env:production`, and may be useful for forward
+   * compatibility with Automat's `development` | `production`.
    */
   environment?: string;
 
