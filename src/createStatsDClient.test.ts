656e7c8321e6d585cd46bc225d22e1afb5a2d131
Ryan Ling <ryan@outlook.com.au>
2025-08-10 08:13:32 +1000

    Add tests

--- src/createStatsDClient.test.ts
+++ src/createStatsDClient.test.ts
@@ -7,7 +7,6 @@ describe('createStatsDClient', () => {
     expect(
       createStatsDClient(StatsD, {
         name: 'test',
-        environment: 'deprecated-but-still-here',
       }),
     ).toBeInstanceOf(Object);
   });
@@ -15,9 +14,50 @@ describe('createStatsDClient', () => {
   it('should handle null config values', () => {
     expect(
       createStatsDClient(StatsD, {
-        name: 'test',
         metricsServer: null,
+        name: 'test',
       }),
     ).toBeInstanceOf(Object);
+  });
+
+  it('should support the environment config option', () => {
+    process.env.DD_ENV = 'my_env';
+    process.env.DD_SERVICE = 'my_service';
+    process.env.DD_VERSION = 'my_version';
+
+    const client = createStatsDClient(StatsD, {
+      environment: 'deprecated-but-still-here',
+      metricsServer: null,
+      name: 'test',
+    });
+
+    client.distribution('dist', 2);
+
+    expect(client.mockBuffer).toMatchInlineSnapshot(`
+      [
+        "test.dist:2|d|#env:deprecated-but-still-here",
+      ]
+    `);
+  });
+
+  it('should allow reading Datadog tags from environment', () => {
+    process.env.DD_ENV = 'my_env';
+    process.env.DD_SERVICE = 'my_service';
+    process.env.DD_VERSION = 'my_version';
+
+    const client = createStatsDClient(StatsD, {
+      environment: 'dd_env_takes_precedence_over_me',
+      includeDataDogTags: true,
+      metricsServer: null,
+      name: 'test',
+    });
+
+    client.increment('counter');
+
+    expect(client.mockBuffer).toMatchInlineSnapshot(`
+      [
+        "test.counter:1|c|#env:my_env,service:my_service,version:my_version",
+      ]
+    `);
   });
 });
